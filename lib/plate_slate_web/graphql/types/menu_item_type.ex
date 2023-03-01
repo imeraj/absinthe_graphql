3dae984c8c5be6b3476a4cdb70459115f34dff01
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:05 -0500

    scalar types added non-null types added

--- lib/plate_slate_web/graphql/types/menu_item_type.ex
+++ lib/plate_slate_web/graphql/types/menu_item_type.ex
@@ -3,10 +3,15 @@ defmodule PlateSlateWeb.Graphql.Types.MenuItemType do
 
   use Absinthe.Schema.Notation
 
+  import_types(PlateSlateWeb.Graphql.Types.Scalars.Date)
+  import_types(PlateSlateWeb.Graphql.Types.Scalars.Decimal)
+
   @desc "A menu item"
   object :menu_item do
-    field :id, :id, description: "Menu item ID"
-    field :name, :string, description: "Menu item name"
+    field :id, non_null(:id), description: "Menu item ID"
+    field :name, non_null(:string), description: "Menu item name"
     field :description, :string, description: "Menu item description"
+    field :price, non_null(:decimal), description: "Menu item price"
+    field :added_on, non_null(:date), description: "Menu item added on date"
   end
 end
