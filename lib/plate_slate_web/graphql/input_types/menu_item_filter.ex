641f296702de3421d9de9f152e4a35a0d18fe1a8
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:12:54 -0500

    add menu item filter and sort order add tests refactor code

--- lib/plate_slate_web/graphql/input_types/menu_item_filter.ex
+++ lib/plate_slate_web/graphql/input_types/menu_item_filter.ex
@@ -2,10 +2,23 @@ defmodule PlateSlateWeb.Graphql.InputTypes.MenuItemFilter do
   @moduledoc false
 
   use Absinthe.Schema.Notation
+  import_types(PlateSlateWeb.Graphql.Types.Enums.SortOrderEnum)
 
   @desc "Filtering options for the menu items list"
   input_object :menu_item_filter do
     @desc "Matching a name"
     field :name, :string
+
+    @desc "Matching a category name"
+    field :category, :string
+
+    @desc "Matching a tag"
+    field :tag, :string
+
+    @desc "Priced above a value"
+    field :price_above, :float
+
+    @desc "Priced below a value"
+    field :price_below, :float
   end
 end
--- lib/plate_slate_web/graphql/queries/menu_query.ex
+++ lib/plate_slate_web/graphql/queries/menu_query.ex
@@ -10,7 +10,13 @@ defmodule PlateSlateWeb.Graphql.Queries.MenuQuery do
   object :menu_query do
     @desc "Get a menu of items"
     field :menu, :menu_payload do
-      arg(:filter, :menu_item_filter, description: "Filters for menu items")
+      arg(:filter, :menu_item_filter, description: "Filter for menu items")
+
+      arg(:order, :sort_order,
+        default_value: :asc,
+        description: "Menu items sort order. Default ASC"
+      )
+
       resolve(&MenuResolver.menu_items/3)
     end
   end
