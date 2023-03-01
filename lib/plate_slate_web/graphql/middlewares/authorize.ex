1b21587f1dc52d765140ef2daac7c00a5d37ee87
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:30 -0500

    authorization for mutations and subscriptions

---
+++ lib/plate_slate_web/graphql/middlewares/authorize.ex
@@ -0,0 +1,18 @@
+defmodule PlateSlateWeb.Middlewares.Authorize do
+  @moduledoc false
+
+  @behaviour Absinthe.Middleware
+
+  def call(resolution, role) do
+    with %{current_user: current_user} <- resolution.context,
+         true <- correct_role?(current_user, role) do
+      resolution
+    else
+      _ -> Absinthe.Resolution.put_result(resolution, {:error, "unauthorized"})
+    end
+  end
+
+  defp correct_role?(%{role: role}, role), do: true
+  defp correct_role?(%{}, :any), do: true
+  defp correct_role?(_, _), do: false
+end
