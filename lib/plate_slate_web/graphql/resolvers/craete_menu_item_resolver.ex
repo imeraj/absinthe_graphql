ca57d4e3bb60682979c302bae34dc48b58e949f6
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:46 -0500

    handle errors with union

--- lib/plate_slate_web/graphql/resolvers/craete_menu_item_resolver.ex
+++ lib/plate_slate_web/graphql/resolvers/craete_menu_item_resolver.ex
@@ -4,11 +4,27 @@ defmodule PlateSlateWeb.Graphql.Resolvers.CreateMenuItemResolver do
 
   def create_menu_item(_, %{input: params}, _) do
     case Menu.create_item(params) do
-      {:error, _} ->
-        {:error, "could not create menu item"}
+      {:error, changeset} ->
+        {:ok, %{errors: transform_errors(changeset)}}
 
-      {:ok, _} = success ->
-        success
+      {:ok, menu_item} ->
+        # this is because menu_item is a basic type. We don't need to return a map
+        {:ok, menu_item}
     end
+  end
+
+  defp transform_errors(changeset) do
+    changeset
+    |> Ecto.Changeset.traverse_errors(&format_error/1)
+    |> Enum.map(fn
+      {key, value} ->
+        %{key: key, message: value}
+    end)
+  end
+
+  defp format_error({msg, opts}) do
+    Enum.reduce(opts, msg, fn {key, value}, acc ->
+      String.replace(acc, "%{#{key}}", to_string(value))
+    end)
   end
 end
