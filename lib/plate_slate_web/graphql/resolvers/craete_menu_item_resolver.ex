35435fa862ae5cddfd7922071650ec92040860de
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:11:20 -0500

    inline context auth check added tests

--- lib/plate_slate_web/graphql/resolvers/craete_menu_item_resolver.ex
+++ lib/plate_slate_web/graphql/resolvers/craete_menu_item_resolver.ex
@@ -3,10 +3,16 @@ defmodule PlateSlateWeb.Graphql.Resolvers.CreateMenuItemResolver do
 
   alias PlateSlate.Menu
 
-  def create_menu_item(_, %{input: params}, _) do
-    with {:ok, menu_item} <- Menu.create_item(params) do
-      # this is because menu_item is a basic type. We don't need to return a map
-      {:ok, menu_item}
+  def create_menu_item(_, %{input: params}, %{context: context}) do
+    case context do
+      %{current_user: %{role: "employee"}} ->
+        with {:ok, menu_item} <- Menu.create_item(params) do
+          # this is because menu_item is a basic type. We don't need to return a map
+          {:ok, menu_item}
+        end
+
+      _ ->
+        {:error, "unauthorized"}
     end
   end
 end
