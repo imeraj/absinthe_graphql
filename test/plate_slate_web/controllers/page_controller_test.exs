8d4b793f4b54de120c0825bcbc7e8def57420002
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:28 -0500

    App skeleton

---
+++ test/plate_slate_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule PlateSlateWeb.PageControllerTest do
+  use PlateSlateWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
