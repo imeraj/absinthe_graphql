0d752828b84831750f23a423154551454772433f
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:47 -0500

    inline context auth check added tests

--- test/support/fixtures/fixtures.ex
+++ test/support/fixtures/fixtures.ex
@@ -2,7 +2,7 @@ defmodule PlateSlate.Fixtures do
   @moduledoc false
 
   def create_user(attrs \\ %{}) do
-    int = :erlang.unique_integer([:positive, :monotonic])
+    int = System.unique_integer([:positive, :monotonic])
 
     {:ok, user} =
       Map.merge(
