89a75bd2d20512b624100a747983dcf9f3b4322a
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:47 -0500

    add Debug middleware

--- lib/plate_slate_web/graphql/types/menu_item_type.ex
+++ lib/plate_slate_web/graphql/types/menu_item_type.ex
@@ -1,6 +1,8 @@
 defmodule PlateSlateWeb.Graphql.Types.MenuItemType do
   @moduledoc false
 
+  import Ecto.Query, warn: false
+
   use Absinthe.Schema.Notation
 
   @desc "A menu item"
@@ -10,5 +12,12 @@ defmodule PlateSlateWeb.Graphql.Types.MenuItemType do
     field :description, :string, description: "Menu item description"
     field :price, non_null(:decimal), description: "Menu item price"
     field :added_on, non_null(:date), description: "Menu item added on date"
+
+    field :category, :category do
+      resolve(fn menu_item, _, _ ->
+        query = Ecto.assoc(menu_item, :category)
+        {:ok, PlateSlate.Repo.one(query)}
+      end)
+    end
   end
 end
