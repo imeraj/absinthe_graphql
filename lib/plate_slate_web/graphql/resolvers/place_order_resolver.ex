786779daa05fe23c81ad65ec45e8a08e7cc92747
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:37 -0500

    create place_order_mutation

---
+++ lib/plate_slate_web/graphql/resolvers/place_order_resolver.ex
@@ -0,0 +1,17 @@
+defmodule PlateSlateWeb.Graphql.Resolvers.PlaceOrderResolver do
+  @moduledoc false
+
+  alias PlateSlateWeb.Utils
+  alias PlateSlate.Ordering
+
+  def place_order(_, %{input: params}, _) do
+    case Ordering.create_order(params) do
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:ok, %{errors: Utils.transform_errors(changeset)}}
+
+      {:ok, order} ->
+        # this is because menu_item is a basic type. We don't need to return a map
+        {:ok, order}
+    end
+  end
+end
