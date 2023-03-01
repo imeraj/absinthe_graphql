e4cabce8821a023e79c98ac144fca3d4c09ce87d
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:12:56 -0500

    use login in admin

---
+++ lib/plate_slate_web/plugs/admin_auth.ex
@@ -0,0 +1,22 @@
+defmodule PlateSlateWeb.Plugs.AdminAuth do
+  @moduledoc false
+
+  @behaviour Plug
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _) do
+    with id when not is_nil(id) <- get_session(conn, :employee_id),
+         %{} = user <- PlateSlate.Accounts.lookup("employee", id) do
+      conn
+      |> Plug.Conn.assign(:current_user, user)
+      |> Absinthe.Plug.put_options(context: %{current_user: user})
+    else
+      _ ->
+        conn
+        |> clear_session
+        |> Phoenix.Controller.redirect(to: "/admin/session/new")
+    end
+  end
+end
