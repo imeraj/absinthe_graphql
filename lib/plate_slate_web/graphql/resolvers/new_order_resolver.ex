a94517ff0691e330362dc7aa9fa2c462ad1a3b53
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:13 -0500

    subscription test added

--- lib/plate_slate_web/graphql/resolvers/new_order_resolver.ex
+++ lib/plate_slate_web/graphql/resolvers/new_order_resolver.ex
@@ -2,7 +2,6 @@ defmodule PlateSlateWeb.Graphql.Resolvers.NewOrderResolver do
   @moduledoc false
 
   def new_order(order, _, _) do
-    IO.inspect(order, label: "New order")
     {:ok, order}
   end
 end
