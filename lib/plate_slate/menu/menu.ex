8088349ca1efd7dbeea26ef779500a9c856e9cba
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:12 -0500

    refactor to have proper directory and code structure

--- lib/plate_slate/menu/menu.ex
+++ lib/plate_slate/menu/menu.ex
@@ -8,7 +8,7 @@ defmodule PlateSlate.Menu do
   alias PlateSlate.Repo
   alias PlateSlate.Menu.Item
 
-  def menu_items(%{matching: name}) when is_binary(name) do
+  def menu_items(%{filter: %{name: name}}) when is_binary(name) do
     query = from t in Item, where: ilike(t.name, ^"%#{name}%")
     Repo.all(query)
   end
