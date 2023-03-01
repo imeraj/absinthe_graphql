f35e44eb9dfa1c314dfd4c5350417e2b0c0d3174
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:11:18 -0500

    scalar types added non-null types added

---
+++ lib/plate_slate_web/graphql/types/scalars/decimal.ex
@@ -0,0 +1,18 @@
+defmodule PlateSlateWeb.Graphql.Types.Scalars.Decimal do
+  @moduledoc false
+
+  use Absinthe.Schema.Notation
+
+  scalar :decimal do
+    parse(fn
+      %{value: value} ->
+        {decimal, _} = Decimal.parse(value)
+        decimal
+
+      _ ->
+        :error
+    end)
+
+    serialize(&to_string/1)
+  end
+end
