331dfc09c66735e6b9b4eadfaf3804b33272de82
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:13 -0500

    add mutation test

---
+++ test/plate_slate_web/schema/mutations/create_menu_item_test.exs
@@ -0,0 +1,55 @@
+defmodule PlateSlateWeb.Schema.Mutations.CreateMenuTest do
+  use PlateSlateWeb.ConnCase, async: true
+
+  alias PlateSlate.{Repo, Menu}
+  import Ecto.Query
+
+  setup context do
+    PlateSlate.Seeds.run()
+
+    query =
+      from c in Menu.Category,
+        select: c.id,
+        where:
+          c.name ==
+            "Sandwiches"
+
+    category_id = Repo.one!(query)
+
+    {:ok, category_id: category_id, conn: context.conn}
+  end
+
+  @query """
+  mutation CreateMenuItem($menuItem: CreateMenuItemInput!) {
+    createMenuItem(input: $menuItem) {
+      menuItem {
+        name
+        description
+        price
+      }
+    }
+  }
+  """
+  test "createMenuItem creates an item", %{category_id: category_id, conn: conn} do
+    menu_item = %{
+      "name" => "French Dip",
+      "description" => "Raost beef, caramelized onions, ...",
+      "price" => 5.75,
+      "category_id" => category_id
+    }
+
+    conn = post(conn, "/api", query: @query, variables: %{"menuItem" => menu_item})
+
+    assert %{
+             "data" => %{
+               "createMenuItem" => %{
+                 "menuItem" => %{
+                   "description" => "Raost beef, caramelized onions, ...",
+                   "name" => "French Dip",
+                   "price" => "5.75"
+                 }
+               }
+             }
+           } = json_response(conn, 200)
+  end
+end
