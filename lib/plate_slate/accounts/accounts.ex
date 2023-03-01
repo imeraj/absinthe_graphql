2602f79344a92b2d88c529b8a0ed5008ffd65f68
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:12:55 -0500

    signup mutation added

---
+++ lib/plate_slate/accounts/accounts.ex
@@ -0,0 +1,27 @@
+defmodule PlateSlate.Accounts do
+  @moduledoc false
+
+  import Ecto.Query, warn: false
+
+  alias PlateSlate.Repo
+  alias Comeonin.Ecto.Password
+  alias PlateSlate.Accounts.User
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def authenticate(role, email, password) do
+    user = Repo.get_by(User, role: to_string(role), email: email)
+
+    with %{password: digest} <- user,
+         true <- Password.valid?(password, digest) do
+      {:ok, user}
+    else
+      _ ->
+        {:error, "incorrect email or password"}
+    end
+  end
+end
