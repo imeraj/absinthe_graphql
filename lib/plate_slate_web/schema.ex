bf90c5cfc3f3704fc94371a60bb6a808e51b5e07
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:13 -0500

    create place_order_mutation

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -6,21 +6,25 @@ defmodule PlateSlateWeb.Schema do
 
   import_types(PlateSlateWeb.Graphql.Types.Enums.SortOrderEnum)
 
-  import_types(PlateslateWeb.Graphql.Types.Error)
+  import_types(PlateSlateWeb.Graphql.Types.Error)
   import_types(PlateSlateWeb.Graphql.Types.MenuItemType)
   import_types(PlateSlateWeb.Graphql.Types.CategoryType)
+  import_types(PlateSlateWeb.Graphql.Types.OrderType)
 
   import_types(PlateSlateWeb.Graphql.InputTypes.MenuItemFilter)
   import_types(PlateSlateWeb.Graphql.InputTypes.CreateMenuItemInput)
+  import_types(PlateSlateWeb.Graphql.InputTypes.PlaceOrderInput)
 
   import_types(PlateSlateWeb.Graphql.Queries.MenuQuery)
   import_types(PlateSlateWeb.Graphql.Queries.SearchQuery)
 
   import_types(PlateSlateWeb.Graphql.Mutations.CreateMenuItemMutation)
+  import_types(PlateSlateWeb.Graphql.Mutations.PlaceOrderMutation)
 
   import_types(PlateSlateWeb.Graphql.Payloads.MenuPayload)
   import_types(PlateSlateWeb.Graphql.Payloads.SearchPayload)
   import_types(PlateSlateWeb.Graphql.Payloads.CreateMenuItemPayload)
+  import_types(PlateSlateWeb.Graphql.Payloads.PlaceOrderPayload)
 
   query do
     import_fields(:menu_query)
@@ -29,5 +33,6 @@ defmodule PlateSlateWeb.Schema do
 
   mutation do
     import_fields(:create_menu_item_mutation)
+    import_fields(:place_order_mutation)
   end
 end
