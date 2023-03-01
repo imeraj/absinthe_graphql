8babf67cfd1074f1cde621debd67d27ee8ddb9f3
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:14:19 -0500

    add create_menu_item mutation

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -4,19 +4,29 @@ defmodule PlateSlateWeb.Schema do
   import_types(PlateSlateWeb.Graphql.Types.Scalars.Date)
   import_types(PlateSlateWeb.Graphql.Types.Scalars.Decimal)
 
+  import_types(PlateSlateWeb.Graphql.Types.Enums.SortOrderEnum)
+
   import_types(PlateSlateWeb.Graphql.Types.MenuItemType)
   import_types(PlateSlateWeb.Graphql.Types.CategoryType)
 
   import_types(PlateSlateWeb.Graphql.InputTypes.MenuItemFilter)
+  import_types(PlateSlateWeb.Graphql.InputTypes.CreateMenuItemInput)
 
   import_types(PlateSlateWeb.Graphql.Queries.MenuQuery)
   import_types(PlateSlateWeb.Graphql.Queries.SearchQuery)
 
+  import_types(PlateSlateWeb.Graphql.Mutations.CreateMenuItemMutation)
+
   import_types(PlateSlateWeb.Graphql.Payloads.MenuPayload)
   import_types(PlateSlateWeb.Graphql.Payloads.SearchPayload)
+  import_types(PlateSlateWeb.Graphql.Payloads.CreateMenuItemPayload)
 
   query do
     import_fields(:menu_query)
     import_fields(:search_query)
+  end
+
+  mutation do
+    import_fields(:create_menu_item_mutation)
   end
 end
