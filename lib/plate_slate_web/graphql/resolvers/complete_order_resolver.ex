ab6ae70f51b01583a848ec529e5409b1373a5e9b
Meraj <meraj.enigma@gmail.com>
2023-03-01 07:12:55 -0500

    implemented changeset errors handler

--- lib/plate_slate_web/graphql/resolvers/complete_order_resolver.ex
+++ lib/plate_slate_web/graphql/resolvers/complete_order_resolver.ex
@@ -1,18 +1,15 @@
 defmodule PlateSlateWeb.Graphql.Resolvers.CompleteOrderResolver do
   @moduledoc false
 
-  alias PlateSlateWeb.Utils
   alias PlateSlate.Ordering
+  alias PlateSlate.Ordering.Order
 
   def complete_order(_, %{id: id}, _) do
-    order = Ordering.get_order!(id)
-
-    case Ordering.update_order(order, %{state: "complete"}) do
-      {:ok, order} ->
-        {:ok, order}
-
-      {:error, changeset} ->
-        {:ok, %{errors: Utils.transform_errors(changeset)}}
+    with %Order{} = order <- Ordering.get_order(id),
+         {:ok, order} <- Ordering.update_order(order, %{state: "complete"}) do
+      {:ok, order}
+    else
+      nil -> {:error, "invalid ID"}
     end
   end
 end
